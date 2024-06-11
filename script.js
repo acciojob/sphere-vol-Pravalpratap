b085c8ccf591e3b527ea4232ad4ec55598f5f0ca
Praval Pratap Singh <113175059+Pravalpratap@users.noreply.github.com>
2024-06-11 06:36:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
-
+function volume_sphere(event) {
+            event.preventDefault(); 
+            
+            let radius = parseFloat(document.getElementById("radius").value);
+            let volume = document.getElementById("volume");
+            
+          
+            if (isNaN(radius) || radius < 0) {
+                volume.value = "Invalid input. Please enter a non-negative number.";
+                return;
+            }
+            
+           
+            let result = (4 / 3) * Math.PI * Math.pow(radius, 3);
+            
+           
+            volume.value = result.toFixed(4);
+        }
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
